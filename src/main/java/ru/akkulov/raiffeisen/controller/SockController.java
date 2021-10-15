d85702895d7bc120126e81d6d12f963d8bc997ab
Omar Akkulov <omar-akkulov@mail.ru>
2021-10-15 03:50:06 +0300

    add Swagger to project

--- src/main/java/ru/akkulov/raiffeisen/controller/SockController.java
+++ src/main/java/ru/akkulov/raiffeisen/controller/SockController.java
@@ -1,5 +1,6 @@
 package ru.akkulov.raiffeisen.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +16,19 @@ public class SockController {
     private final SockService sockService;
 
     @PostMapping("/income")
+    @ApiOperation(value = "Create Sock")
     public ResponseEntity<Sock> addSock(@RequestBody Sock sock) {
         return ResponseEntity.ok(sockService.createSock(sock));
     }
 
     @PostMapping("/outcome")
+    @ApiOperation(value = "Remove socks from stock")
     public ResponseEntity<Sock> outComeSocks(@RequestBody Sock comingSock) {
         return ResponseEntity.ok(sockService.getSockByColorAndCottonPartAndOutcome(comingSock));
     }
 
     @GetMapping
+    @ApiOperation(value = "Get socks quantity by request parameters")
     public ResponseEntity<String> getQuantityByParameters(@RequestParam String color,
                                                           @RequestParam Operation operation,
                                                           @RequestParam int cottonPart) {
@@ -33,11 +37,13 @@ public class SockController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get Sock by Id")
     public ResponseEntity<Sock> getSocksById(@PathVariable long id) {
         return ResponseEntity.ok(sockService.getSocksById(id));
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete Sock by Id")
     public ResponseEntity<Sock> deleteSocksById(@PathVariable long id) {
         sockService.deleteSocksById(id);
         return new ResponseEntity<>(HttpStatus.OK);
